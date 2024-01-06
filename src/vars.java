26a86b9cfdff6171d8bb52383ee2c542af9e8329
thai-ru <robinthairu34@gmail.com>
2024-01-06 07:24:26 +0300

    constant, variables

---
+++ src/vars.java
@@ -0,0 +1,11 @@
+public class vars {
+    public static void main(String[] args) {
+        double salary;
+        salary = 45999;
+        System.out.println(salary);
+
+        int vacationDays;
+        vacationDays = 12;
+        System.out.println(vacationDays);
+    }
+}
